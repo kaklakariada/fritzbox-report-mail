438013624e34bce69c01f0fdb5a72cc90af35fc0
kaklakariada <kaklakariada@users.noreply.github.com>
2022-01-23 17:19:14 +0100

    Cleanup

--- app/src/main/java/Config.java
+++ app/src/main/java/Config.java
@@ -1,22 +1,3 @@
-/**
- * A Java API for parsing and processing status report mails of a FritzBox
- * Copyright (C) 2018 Christoph Pirkl <christoph at users.sourceforge.net>
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
-
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.UncheckedIOException;
@@ -51,10 +32,6 @@ public class Config {
 
 	public String getJdbcPassword() {
 		return properties.getProperty("jdbc.password");
-	}
-
-	public String getJdbcSchemaName() {
-		return properties.getProperty("jdbc.schema");
 	}
 
 	public static Config readConfig(Path path) {
--- app/src/main/java/ParseReportMails.java
+++ app/src/main/java/ParseReportMails.java
@@ -1,32 +1,8 @@
-
-/**
- * A Java API for parsing and processing status report mails of a FritzBox
- * Copyright (C) 2018 Christoph Pirkl <christoph at users.sourceforge.net>
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import com.github.kaklakariada.fritzbox.report.ReportService;
 import com.github.kaklakariada.fritzbox.report.model.FritzBoxReportCollection;
 import com.github.kaklakariada.serialization.KryoSerializerService;
 
 public class ParseReportMails {
-    private static final Logger LOG = LoggerFactory.getLogger(ParseReportMails.class);
-
     public static void main(final String[] args) {
         final Config config = Config.readConfig(DbImporter.CONFIG_FILE);
         FritzBoxReportCollection reportCollection;
