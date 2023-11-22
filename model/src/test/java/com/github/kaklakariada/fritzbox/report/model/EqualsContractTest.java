f39ed857822cad74f7a944896db0deeedfa004bf
kaklakariada <kaklakariada@users.noreply.github.com>
2023-11-22 19:34:49 +0100

    Add toString tests

--- model/src/test/java/com/github/kaklakariada/fritzbox/report/model/EqualsContractTest.java
+++ model/src/test/java/com/github/kaklakariada/fritzbox/report/model/EqualsContractTest.java
@@ -1,34 +1,38 @@
 package com.github.kaklakariada.fritzbox.report.model;
 
-import com.github.kaklakariada.fritzbox.report.model.event.DslSyncFailed;
-import com.github.kaklakariada.fritzbox.report.model.event.DslSyncSuccessful;
-import com.github.kaklakariada.fritzbox.report.model.event.IPv4InternetConnectionEstablished;
-import com.github.kaklakariada.fritzbox.report.model.event.IPv6InternetConnectionEstablished;
-import com.github.kaklakariada.fritzbox.report.model.event.IPv6PrefixAssigned;
-import com.github.kaklakariada.fritzbox.report.model.event.InternetDisconnected;
-import com.github.kaklakariada.fritzbox.report.model.event.WifiDeviceAuthorizationFailed;
-import com.github.kaklakariada.fritzbox.report.model.event.WifiDeviceConnected;
-import com.github.kaklakariada.fritzbox.report.model.event.WifiDeviceDisconnected;
-import com.github.kaklakariada.fritzbox.report.model.event.WifiDeviceDisconnectedHard;
-import com.github.kaklakariada.fritzbox.report.model.event.WifiGuestDeviceConnected;
-import com.github.kaklakariada.fritzbox.report.model.event.WifiGuestDeviceDisconnected;
-import com.github.kaklakariada.fritzbox.report.model.event.WifiGuestDeviceDisconnectedHard;
+import java.util.stream.Stream;
 
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import com.github.kaklakariada.fritzbox.report.model.event.*;
+import com.jparams.verifier.tostring.ToStringVerifier;
 
 import nl.jqno.equalsverifier.EqualsVerifier;
 import nl.jqno.equalsverifier.Warning;
 
 class EqualsContractTest {
     @ParameterizedTest
-    @ValueSource(classes = { DslSyncFailed.class, DslSyncSuccessful.class, InternetDisconnected.class,
-            IPv4InternetConnectionEstablished.class, IPv6InternetConnectionEstablished.class, IPv6PrefixAssigned.class,
-            WifiDeviceAuthorizationFailed.class, WifiDeviceConnected.class, WifiDeviceDisconnected.class,
-            WifiDeviceDisconnectedHard.class, WifiGuestDeviceConnected.class, WifiGuestDeviceDisconnected.class,
-            WifiGuestDeviceDisconnectedHard.class, //
-            DataConnections.class, AggregatedVolume.class, DataVolume.class })
-    void equalsContract(Class<?> type) {
+    @MethodSource("modelClasses")
+    void equalsContract(final Class<?> type) {
         EqualsVerifier.forClass(type).suppress(Warning.STRICT_INHERITANCE).verify();
     }
+
+    @ParameterizedTest
+    @MethodSource("modelClasses")
+    void toStringContract(final Class<?> type) {
+        ToStringVerifier.forClass(type).verify();
+    }
+
+    static Stream<Arguments> modelClasses() {
+        return Stream.of(DslSyncFailed.class, DslSyncSuccessful.class, InternetDisconnected.class,
+                IPv4InternetConnectionEstablished.class, IPv6InternetConnectionEstablished.class,
+                IPv6PrefixAssigned.class,
+                WifiDeviceAuthorizationFailed.class, WifiDeviceConnected.class, WifiDeviceDisconnected.class,
+                WifiDeviceDisconnectedHard.class, WifiGuestDeviceConnected.class, WifiGuestDeviceDisconnected.class,
+                WifiGuestDeviceDisconnectedHard.class, //
+                DataConnections.class, AggregatedVolume.class, DataVolume.class).map(Arguments::of);
+    }
+
 }
